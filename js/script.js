8b3d678e3619ae202190c2de9f6146b1a550d96f
joseph.kwon <joseph.kwon@cj.net>
2024-11-09 16:41:15 +0900

    팝업 내용변경스크립트 적용 및 임시 데이터 연결

--- js/script.js
+++ js/script.js
@@ -118,32 +118,150 @@ $(document).ready(function () {
   //   }, 300)
   // );
 
+  // section04 popup Data
+  const popupData = [
+      {
+        title : "title01",
+        discription : "discription01",
+        images : [
+          "portfolio_rename/(10부스) 2024 대한민국교육박람회 - NHN에듀/NHN (1).jpg",
+          "portfolio_rename/(10부스) 2024 대한민국교육박람회 - NHN에듀/NHN (2).jpg",
+          "portfolio_rename/(10부스) 2024 대한민국교육박람회 - NHN에듀/NHN에듀 (5).jpg",
+          "portfolio_rename/(10부스) 2024 대한민국교육박람회 - NHN에듀/NHN에듀 (6).jpg"
+        ]
+      },
+      {
+        title : "title02",
+        discription : "discription02",
+        images : [
+          "portfolio_rename/(10부스) 국제기능올림픽 특별대회 - 한국산업인력공단/한국산업인력공단-01.jpg",
+          "portfolio_rename/(10부스) 국제기능올림픽 특별대회 - 한국산업인력공단/한국산업인력공단-02.jpg",
+          "portfolio_rename/(10부스) 국제기능올림픽 특별대회 - 한국산업인력공단/KakaoTalk_20221020_164640929_14.jpg",
+          "portfolio_rename/(10부스) 국제기능올림픽 특별대회 - 한국산업인력공단/KakaoTalk_20221103_161050032.jpg"
+        ]
+      },
+      {
+        title : "title03",
+        discription : "discription03",
+        images : [
+          "portfolio_rename/(10부스) 대한민국 교육박람회 - 빅드림/꾸그&티처스 1228 1.jpg",
+          "portfolio_rename/(10부스) 대한민국 교육박람회 - 빅드림/꾸그&티처스 1228 6.jpg",
+          "portfolio_rename/(10부스) 대한민국 교육박람회 - 빅드림/KakaoTalk_20230113_095401466_06.jpg",
+          "portfolio_rename/(10부스) 대한민국 교육박람회 - 빅드림/KakaoTalk_20230113_095401466_09.jpg"
+        ]
+      },
+      {
+        title : "title04",
+        discription : "discription04",
+        images : [
+          "portfolio_rename/(12부스) 2022탄소중립 EXPO - 경기테크노파크/경기TP-1.jpg",
+          "portfolio_rename/(12부스) 2022탄소중립 EXPO - 경기테크노파크/경기TP-3.jpg",
+          "portfolio_rename/(12부스) 2022탄소중립 EXPO - 경기테크노파크/KakaoTalk_20221102_144109703_01.jpg",
+          "portfolio_rename/(12부스) 2022탄소중립 EXPO - 경기테크노파크/KakaoTalk_20221102_144109703_03.jpg"
+        ]
+      },
+      {
+        title : "title05",
+        discription : "discription05",
+        images : [
+          "portfolio_rename/(15부스) 2022 IFS프랜차이즈 - 로보테크/로보테크 0929 1.jpg",
+          "portfolio_rename/(15부스) 2022 IFS프랜차이즈 - 로보테크/로보테크 0929 4.jpg",
+          "portfolio_rename/(15부스) 2022 IFS프랜차이즈 - 로보테크/KakaoTalk_20221020_105439076_05.jpg",
+          "portfolio_rename/(15부스) 2022 IFS프랜차이즈 - 로보테크/KakaoTalk_20221020_105439076_09.jpg"
+        ]
+      },
+      {
+        title : "title06",
+        discription : "discription06",
+        images : [
+          "portfolio_rename/(15부스) 2022 IFS프랜차이즈 - 코리아런드리/코리아런드리 _View04.jpg",
+          "portfolio_rename/(15부스) 2022 IFS프랜차이즈 - 코리아런드리/코리아런드리 _View05.jpg",
+          "portfolio_rename/(15부스) 2022 IFS프랜차이즈 - 코리아런드리/KakaoTalk_20221020_105439076_18.jpg",
+          "portfolio_rename/(15부스) 2022 IFS프랜차이즈 - 코리아런드리/KakaoTalk_20221020_105439076.jpg"
+        ]
+      },
+      {
+        title : "title07",
+        discription : "discription07",
+        images : [
+          "portfolio_rename/(20부스) 2024 경기국제보트쇼 - 경기바다/평택항만공사 (2).jpg",
+          "portfolio_rename/(20부스) 2024 경기국제보트쇼 - 경기바다/평택항만공사 (3).jpg",
+          "portfolio_rename/(20부스) 2024 경기국제보트쇼 - 경기바다/평택항만공사-01.jpg",
+          "portfolio_rename/(20부스) 2024 경기국제보트쇼 - 경기바다/평택항만공사-02.jpg"
+        ]
+      },
+      {
+        title : "title08",
+        discription : "discription08",
+        images : [
+          "portfolio_rename/(20부스) 2024 대한민국교육박람회 - EBS/EBS (3).jpg",
+          "portfolio_rename/(20부스) 2024 대한민국교육박람회 - EBS/EBS (5).jpg",
+          "portfolio_rename/(20부스) 2024 대한민국교육박람회 - EBS/EBS(1).jpg",
+          "portfolio_rename/(20부스) 2024 대한민국교육박람회 - EBS/EBS(3).jpg"
+        ]
+      },
+      {
+        title : "title09",
+        discription : "discription09",
+        images : [
+          "portfolio_rename/(20부스) AGF 2023 - Bushiroad/부쉬로드 (1).jpg",
+          "portfolio_rename/(20부스) AGF 2023 - Bushiroad/부쉬로드 (2).jpg",
+          "portfolio_rename/(20부스) AGF 2023 - Bushiroad/부쉬로드 (3).jpg",
+          "portfolio_rename/(20부스) AGF 2023 - Bushiroad/부쉬로드 (4).jpg"
+        ]
+      },
+      {
+        title : "title10",
+        discription : "discription10",
+        images : [
+          "portfolio_rename/(60부스) 2024 드론쇼 코리아 - 숨비/숨비 설치사진 (1).jpg",
+          "portfolio_rename/(60부스) 2024 드론쇼 코리아 - 숨비/숨비 설치사진 (2).jpg",
+          "portfolio_rename/(60부스) 2024 드론쇼 코리아 - 숨비/숨비 시안-01.jpg",
+          "portfolio_rename/(60부스) 2024 드론쇼 코리아 - 숨비/숨비 시안-02.jpg"
+        ]
+      }
+  ];
   // section04 팝업 열기
-  var listItem = $(".portfolioItem a")
+  var listItem = $(".portfolioItem")
   var popup = $(".section04 .popup")
   var popupClose = $(".section04 .popup .closeBtn")
-  var popupNaviBtn = $(".section04 .popup .imgNavi a")
+  var popupNaviBtn = $(".section04 .popup .imgNavi")
   var popupMainImg = $(".section04 .popup .mainImg img")
+  // popup item
+  var popupTitle = $(".section04 .popup .title")
+  var popupDic = $(".section04 .popup .discription")
 
   listItem.on("click", function(e){
     e.preventDefault()
-    console.log("click");
+    console.log($(this).index());
     popup.addClass("on")
+    // popupdata 변경
+    var index = $(this).index()
+    var tgData = popupData[index]
+    popupTitle.text(tgData.title)
+    popupDic.text(tgData.discription)
+    popupMainImg.attr("src",tgData.images[0])
+    popupNaviBtn.find("a").eq(0).find("img").attr("src",tgData.images[0])
+    popupNaviBtn.find("a").eq(1).find("img").attr("src",tgData.images[1])
+    popupNaviBtn.find("a").eq(2).find("img").attr("src",tgData.images[2])
+    popupNaviBtn.find("a").eq(3).find("img").attr("src",tgData.images[3])
+    popupNaviBtn.find("a").removeClass("active")
+    popupNaviBtn.find("a").eq(0).addClass("active")
   })
-  popupClose.on("click", function(e){
 
+  popupClose.on("click", function(e){
     console.log("click");
     popup.removeClass("on")
   })
 
   // 팝업 내 이미지 변경
-  popupNaviBtn.on("click", function(e){
+  popupNaviBtn.find("a").on("click", function(e){
     e.preventDefault()
     console.log(this);
     var tgImg = $(this).find('img').attr("src")
     var currentImg = popupMainImg.attr("src")
     popupMainImg.attr("src", tgImg)
-    popupNaviBtn.removeClass("active")
+    popupNaviBtn.find("a").removeClass("active")
     $(this).addClass("active")
   })
 
